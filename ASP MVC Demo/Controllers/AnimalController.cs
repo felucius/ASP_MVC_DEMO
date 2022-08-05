c4d4e6e33c57d03a8039745c2e1c366b2f3677d7
Maxime de Lange <maxime.de.lange@outlook.com>
2022-08-05 12:32:30 +0200

    Added base ASP.NET MVC project

---
+++ ASP MVC Demo/Controllers/AnimalController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP_MVC_Demo.Controllers
+{
+    public class AnimalController : Controller
+    {
+        // GET: /Animal
+        public string Index()
+        {
+            return "This is my <b>default</b> action";
+        }
+
+        /// <summary>
+        /// GET /Animal/Welcome/
+        /// </summary>
+        /// <returns></returns>
+        public string Welcome(string name, int id = 1)
+        {
+            return HttpUtility.HtmlEncode("Hello " + name + ", ID is: " + id);
+        }
+    }
+}
