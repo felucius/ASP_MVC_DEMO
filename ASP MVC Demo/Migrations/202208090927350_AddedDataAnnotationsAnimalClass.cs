c708f1380f9de7631120e6de994eda119f472956
Maxime <maxime.de.lange@outlook.com>
2022-08-09 11:42:56 +0200

    Added DataAnnotations for the Model. Added migrations to correspond with the model

---
+++ ASP MVC Demo/Migrations/202208090927350_AddedDataAnnotationsAnimalClass.cs
@@ -0,0 +1,18 @@
+﻿namespace ASP_MVC_Demo.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedDataAnnotationsAnimalClass : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Animals", "Name", c => c.String(maxLength: 25));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Animals", "Name", c => c.String());
+        }
+    }
+}
---
+++ ASP MVC Demo/Migrations/202208090930067_AddedRegularExpressionFieldForNameProperty.cs
@@ -0,0 +1,16 @@
+﻿namespace ASP_MVC_Demo.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedRegularExpressionFieldForNameProperty : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
